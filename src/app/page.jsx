ee7065379c3ba28117099c255c33bbf6de90b817
Diyorbek92 <diyorbek2017u@gmail.com>
2024-02-16 14:50:34 +0500

    add new invoices buttun

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,17 @@
-// "use client";
-
-// async function getData() {
-//   const req = await fetch("http://localhost:4000/data");
-//   const data = await req.json();
-//   console.log(data);
-//   return data;
-// }
-// getData();
-export default function Home() {
+import { IoAddCircleOutline } from "react-icons/io5";
+async function getData(url) {
+  const req = await fetch(url);
+  const data = await req.json();
+  console.log(data);
+  return data;
+}
+export default async function Home() {
+  const invoices = await getData("http://localhost:4000/data");
   return (
     <main className="max-container">
-      <div className="flex items-center justify-between">
-        <div>
-          <h1 className="text-2xl font-bold">Invoices</h1>
+      <div className="flex gap-44 py-6">
+        <div className="">
+          <h1 className="text-4xl font-bold">Invoices</h1>
           <p className="text-sm opacity-75">There are 7 total invoices</p>
         </div>
         <div>
@@ -23,6 +22,59 @@ export default function Home() {
             <option>Breaking Bad</option>
             <option>Walking Dead</option>
           </select>
+        </div>
+        <div className="">
+          <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              {/* Page content here */}
+              <label
+                htmlFor="my-drawer"
+                className="btn btn-primary drawer-button text-xl rounded-full"
+              >
+                <IoAddCircleOutline />
+                New Invoice
+              </label>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <ul className="menu flex flex-col gap-5 p-4 w-[500px] min-h-full bg-base-200 text-base-content">
+                {/* Sidebar content here */}
+                <li>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </main>
